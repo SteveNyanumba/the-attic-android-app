a4130a4945b86e7e3cc532f8e58cc63ed1fd84ee
Steve Nyanumba <reach@stevenyanumba.com>
2020-12-29 12:10:04 +0300

    2. The Register Activity: Completegit status

---
+++ app/src/main/java/com/steven/theattic/RegisterActivity.java
@@ -0,0 +1,96 @@
+package com.steven.theattic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button registerBtn;
+    private EditText emailField, usernameField, passwordField;
+    private TextView loginTxtView;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference userDetailsReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        loginTxtView = findViewById(R.id.loginTxtView);
+        registerBtn = findViewById(R.id.registerBtn);
+        emailField = findViewById(R.id.emailField);
+        usernameField = findViewById(R.id.usernameField);
+        passwordField = findViewById(R.id.passwordField);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        userDetailsReference = database.getReference().child("Users");
+
+        loginTxtView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this, "LOADING....", Toast.LENGTH_SHORT).show();
+
+                final String username = usernameField.getText().toString();
+                final String email = emailField.getText().toString();
+                final String password = passwordField.getText().toString();
+
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(username)){
+                    if (password.length()<8){
+                        Toast.makeText(RegisterActivity.this, "Password is too short (Less than 8 characters, More Required)", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            String user_id = mAuth.getCurrentUser().getUid();
+                            DatabaseReference current_user_db = userDetailsReference.child(user_id);
+                            current_user_db.child("username").setValue(username);
+                            current_user_db.child("Image").setValue("Default");
+
+                            Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            Intent profIntent = new Intent(RegisterActivity.this, ProfileActivity.class);
+                            profIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            
+
+                        }
+                    });
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Complete All Fields!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+    }
+}
